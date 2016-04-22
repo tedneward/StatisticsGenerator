22e7fb831f0f4409798dd3d53bf71c29eae7857b
Ted Neward <ted@tedneward.com>
2016-04-22 02:09:29 -0700

    First commit, simple (placeholder) tests to make sure infrastructure works

---
+++ StatisticsGenerator/StatGenTests/SimpleTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using StatisticsGenerator;
+
+namespace StatGenTests
+{
+    [TestClass]
+    public class SimpleTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var tested = StatisticsGenerator.Simple.TestMe();
+
+            Assert.AreEqual("I have been tested, and found capable!", tested);
+        }
+    }
+}
---
+++ StatisticsGenerator/StatisticsGenerator/Simple.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StatisticsGenerator
+{
+    public class Simple
+    {
+        public static string TestMe()
+        {
+            return "I have been tested, and found capable!";
+        }
+    }
+}
